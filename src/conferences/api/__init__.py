137e940df02c8af672d9cd818fd53c126c027325
Simon Dalvai <s.dalvai@noi.bz.it>
2024-03-18 16:26:43 +0100

    fix reuse and add ci

--- src/conferences/api/__init__.py
+++ src/conferences/api/__init__.py
@@ -1,4 +1,5 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX-FileCopyrightText: 2023 Digital CUBE <https://digitalcube.rs>
 
 from .conference import *
 
--- src/migrations/models/4_20231109165830_update.py
+++ src/migrations/models/4_20231109165830_update.py
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX-FileCopyrightText: 2023 Digital CUBE <https://digitalcube.rs>
+
 from tortoise import BaseDBAsyncClient
 
 
