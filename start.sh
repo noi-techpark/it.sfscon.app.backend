9c793911aa942d1cb8161444a925ec743e11d294
igor <igor@digitalcube.rs>
2024-10-30 22:56:34 +0000

    Fixing start script

--- start.sh
+++ start.sh
@@ -4,8 +4,7 @@ service cron start
 
 sleep 1
 
-#echo "*/5 * * * * /app/update-conf.py " # date >> /tmp/updates.log && curl --location --request POST 'http://localhost:8000/api/import-xml' --data '' >> /tmp/updates.log && echo "\n" >> /tmp/updates.log" > /tmp/cron
-echo "*/5 * * * * /scripts/update-conf.py " >> /tmp/cron
+echo "*/5 * * * * /scripts/update-conf.py " > /tmp/cron
 crontab /tmp/cron
 
 
